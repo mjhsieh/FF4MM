7686f61809790814af19f2427180e144ad0597f8
Mənzway Shea <mjhsieh@gmail.com>
2011-03-23 10:35:43 -0700

    more about the globals in framework

--- src/framework.f
+++ src/framework.f
@@ -2,39 +2,49 @@
 ! All rights reserved. - please read information in "LICENCSE.txt"
 ! Written by Mengjuei Hsieh, University of California Irvine
 #include "definitions.fpp"
+
+! This will be the main module that should be providing
+! 1. public storage of variables
+! 2. interface for callers
+!
+! Rule 1: Use one dimension array so that it would be easier to
+!         do parallelisation.
 module tienchun
+
+   ! Acquired data
    integer             :: natom
    _REAL_, allocatable ::     x(:)
-   _REAL_, allocatable ::     f(:)
-   _REAL_, allocatable ::     v(:)
-   _REAL_, allocatable ::  oldv(:)
    _REAL_, allocatable ::  mass(:) 
    _REAL_, allocatable :: r_vdw(:)
 
-   ! System Feature
+   ! Acquired or Computed data
+   _REAL_, allocatable ::     f(:)
+   _REAL_, allocatable ::     v(:)
+   _REAL_, allocatable ::  oldv(:)
+
+   ! Computed system states
+   ! RNDF_SOLUTE:   net (real) number of degrees of freedom for solute
+   ! RNDF_SOLVENT:  net (real) number of degrees of freedom for solvent
+   ! RNDF:        total (real) number of degrees of freedom
+
+   ! User Control: Geometry
+   logical             :: usePBC !(periodic boundary condition)
+   logical             :: usePIB !(particle in a box)
+   _REAL_              :: xmin, ymin, zmin
+   _REAL_              :: xmax, ymax, zmax
+
+   ! User Control: Force
    _REAL_              :: vdwcutoff
    _REAL_              :: eelcutoff
-   _REAL_              ::      xmin, ymin, zmin
-   _REAL_              ::      xmax, ymax, zmax
-   ! Program Control
-   logical             :: usePBC
-   logical             :: usePIB !(particle in a box)
+
+   ! User Control: Types of computation
+   logical             :: doMD   !Molecular Dynamics
+!  logical             :: doMIN  !Minimization
+!  logical             :: doMC   !Monte Carlo
+
+   ! User Control: MD and MC
+   _REAL_              :: dt     !timestep, δt
+   _REAL_              :: dt2    !2*δt
+   _REAL_              :: dtsq   !δt**2
 end module tienchun
-
-subroutine force_gravity(atom1,atom2)
-end subroutine force_gravity
-
-subroutine force_littleg
-end subroutine force_littleg
-
-subroutine force_lj(A,B)
-   implicit none
-   _REAL_, dimension(3), intent(in) :: A
-   _REAL_, dimension(3), intent(in) :: B
-end subroutine force_lj
-
-#if 0
-!vectorized Lennard-Jones
-subroutine vdfrclj
-end subroutine vdfrclj
-#endif
+! The origin of module name, TienChun, is a town in central Taiwan.
